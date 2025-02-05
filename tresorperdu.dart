3f5e6046b382609a85846c7aac8b6a1189786edd
modupe <Vous@exemple.com>
2025-02-05 07:09:16 +0100

    enigme

--- tresorperdu.dart
+++ tresorperdu.dart
@@ -1,4 +1,6 @@
 
+import 'dart:io';
+
 class Aventurier{
   //propriete
   String nom;
@@ -11,9 +13,8 @@ class Aventurier{
  void perdreVie() {
   if (vies>0){
     vies--;
-    print("Oups ! Il te reste ${vies} vies.");
+    print("Oups ! Il te reste $vies vies.");
   }
-class 
   if (vies== 0){
     print("la partie est terminée");
   }else{
@@ -21,8 +22,52 @@ class
   }
 }
 }
+ abstract class Obstacle{
+//mathode abstraite que toutes les sous-classes devront implémenter.
+Future<void> affronter ( aventurierjoueur);
+ }
+// sous classe enigme
+class Enigme extends Obstacle{
+  // propriete
+  String question;
+  String reponseCorrecte;
+  //methode
+  @override
+  Future<void> affronter(aventurierjoueur)async{
+    bool reussi= false ;
 
-void main(){
- Aventurier aventurier1= Aventurier("modupe",3);
-  aventurier1.perdreVie(); 
+    while (!reussi && aventurierjoueur.vies > 0) {
+      print(question);
+      String? reponseUtilisateur = stdin.readLineSync();
+
+      if (reponseUtilisateur == reponseCorrecte) {
+        print('${aventurierjoueur.nom} a résolu l\'énigme !');
+        reussi = true;
+      } 
+      else {
+        aventurierjoueur.perdreVie();
+        print('${aventurierjoueur.nom} a donné une mauvaise réponse. Il reste ${aventurierjoueur.vies} vies.');
+        if (aventurierjoueur.vies > 0) {
+          print('Essayez encore !');
+        }
+      }
+      // Attendre un moment avant de redemander
+      await Future.delayed(Duration(seconds: 1));
+
+    if (aventurierjoueur.vies == 0) {
+      print('${aventurierjoueur.nom} n\'a plus de vies. Game Over !');
+    }
+    }
+    
+  }
+  // constructeur 
+  Enigme(this.question,this.reponseCorrecte);
+}
+
+
+void main() async{
+ //Aventurier aventurier1= Aventurier("modupe",3);
+  //aventurier1.perdreVie(); 
+ Enigme enigme=Enigme("Joueur, donnez moi la capitale de Lagos?", 'Ikeja');
+  await enigme.affronter ;
 }
